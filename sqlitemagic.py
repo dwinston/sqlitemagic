85ee5361a11ee84bae032ad134695c4e3b9da805
Ben Waugh <b.waugh@ucl.ac.uk>
2015-02-24 13:06:54 +0000

    Support .schema meta-command

--- sqlitemagic.py
+++ sqlitemagic.py
@@ -8,6 +8,7 @@ select personal, family from person;
 
 produces:
 
+personal|family
 Alan|Turing
 Grace|Hopper
 """
@@ -18,6 +19,7 @@ Grace|Hopper
 # Inspired by https://github.com/tkf/ipython-sqlitemagic.
 
 import sqlite3
+import sys
 from IPython.core.magic import Magics, magics_class, cell_magic
 from IPython.display import display, HTML
 
@@ -30,18 +32,24 @@ class SqliteMagic(Magics):
         connection = sqlite3.connect(filename)
         cursor = connection.cursor()
         try:
-            cursor.execute(query)
-            results = cursor.fetchall()
-            header = [f[0] for f in cursor.description]
-            display(HTML(self.tablify(results, header)))
+	    if query.startswith('.'):
+	        self.meta_command(cursor, query)
+	    else:
+                cursor.execute(query)
+                results = cursor.fetchall()
+                header = [f[0] for f in cursor.description]
+                display(HTML(self.tablify(results, header)))
         except Exception, e:
-            import sys
             print >> sys.stderr, "exception", e
         cursor.close()
         connection.close()
 
-    def tablify(self, rows, header):
-        return '<table>\n' + self.rowify_header(header) + '\n'.join(self.rowify(r) for r in rows) + '\n</table>'
+    def tablify(self, rows, header=None):
+        if header==None:
+	    header_row = ''
+	else:
+	    header_row = self.rowify_header(header)
+        return '<table>\n' + header_row + '\n'.join(self.rowify(r) for r in rows) + '\n</table>'
 
     def rowify(self, row):
         for i, r in enumerate(row):
@@ -52,6 +60,15 @@ class SqliteMagic(Magics):
     def rowify_header(self, row):
         return '<tr>' + ''.join('<th>' + str(r) + '</th>' for r in row) + '</tr>'
 
+    def meta_command(self, cursor, line):
+        parts = line.split()
+        command = parts[0]
+        if command=='.schema':
+	    cursor.execute('select sql from sqlite_master')
+            results = cursor.fetchall()
+            display(HTML(self.tablify(results)))
+        else:
+            print >> sys.stderr, "Unsupported meta-command: ", command
 
 def load_ipython_extension(ipython):
     ipython.register_magics(SqliteMagic)
